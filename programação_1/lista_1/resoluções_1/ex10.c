a7103731e9aac78ff6a62e202ca33c063eccced9
Antonio Dias <antoniompdfilho@gmail.com>
2026-04-07 21:37:54 -0300

    exercicio 10 feito

---
+++ programação_1/lista_1/resoluções_1/ex10.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b,c;
+    float delta, raiz_mais, raiz_menos;
+
+    printf("Informe o coeficiente A: ");
+    scanf("%d", &a);
+    printf("Informe o coeficiente B: ");
+    scanf("%d", &b);
+    printf("Informe o coeficiente C: ");
+    scanf("%d", &c);
+
+    if(a == 0){
+        printf("Nao eh uma equacao do segundo grau");
+
+    }else{
+        delta = (pow(b,2)) - (4 * a * c);
+
+        if(delta < 0){
+            printf("Nao existe raizes reais");
+
+        }else{
+            raiz_mais = (-b + sqrt(delta)) / 2 * a;
+            raiz_menos = (-b - sqrt(delta)) / 2 * a;
+
+            printf("As raizes da equacao sera: %.2f e %.2f", raiz_mais, raiz_menos);
+        }
+    }
+
+    return 0;
+}
