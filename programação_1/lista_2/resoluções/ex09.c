8e09615d0b516e0604afb4cf00f5f4ce45607341
Antonio Dias <antoniompdfilho@gmail.com>
2026-04-20 12:13:34 -0300

    exercicio 9 da lista 2 feito

---
+++ programação_1/lista_2/resoluções/ex09.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main()
+{
+    int candidato1 = 0, candidato2 = 0, candidato3 = 0 , maior = 0, voto = 0, vencedores = 0;
+    
+    while(1){
+        
+        printf("\n[1] - Para o candidato 1\n[2] - Para o candidato 2\n[3] - Para o candidato 3");
+        printf("\nInforme para quem vai seu voto, [0] para encerrar: ");
+        scanf(" %d",&voto);
+        
+        
+        if(voto == 0){
+            break;
+            
+        }else if(voto == 1){
+            candidato1++;
+            
+        }else if(voto == 2){
+            candidato2++;
+            
+        }else if(voto == 3){
+            candidato3++;
+            
+        }else{
+            printf("\nValor invalido\n");
+            
+        }
+        
+    }
+    
+    maior = candidato1;
+    if(candidato2 > maior){
+        maior = candidato2;
+
+    }
+    if(candidato3 > maior){
+        maior = candidato3;
+
+    }
+    
+    printf("\nO candidato 1 recebeu %d votos\nO candidato 2 recebeu %d votos\nO candidato 3 recebeu %d votos",candidato1, candidato2, candidato3);
+
+    if(maior > 0){
+        if(maior == candidato1){
+            vencedores++;
+
+        }
+        if(maior == candidato2){
+            vencedores++;
+            
+        }
+        if(maior == candidato3){
+            vencedores++;
+            
+        }
+    }
+
+    if(vencedores > 1){
+        printf("\nHouve empate");
+
+    }else if(vencedores == 1){
+        if(maior == candidato1){
+            printf("\nO candidato 1 VENCEU");
+            
+        }
+        if(maior == candidato2){
+            printf("\nO candidato 2 VENCEU");
+            
+        }
+        if(maior == candidato3){
+            printf("\nO candidato 3 VENCEU");
+        
+        }
+    }else{
+        printf("\nNinguem votou");
+
+    }
+
+    return 0;
+}
